4d790e8341cee60ea2c8738846592c4340b0b120
MDecker-MobileComputing <mide_42b@yahoo.de>
2026-04-27 17:50:27 +0200

    Update redis-client.js

--- src/redis-client.js
+++ src/redis-client.js
@@ -8,18 +8,13 @@ let redisClient = null;
 
 
 /**
- * Verbindung zu Redis-Server aufbauen.  
+ * Verbindung zu Redis-Server aufbauen.
  */
 export async function initRedisClient() {
 
   logger.info( "Initialisierung Redis-Client ..." );
 
-  redisClient 
-          = await createClient().
-                    on( "error",        (fehler) => logger.info(( `Fehler bei Aufbau Verbindung zu Redis-Server: ${fehler}`) ).
-                    on( "reconnecting", () => logger.info( "Verbindung zum Redis-Server wird wiederhergestellt ..." ) ).
-                    on( "end",          () => logger.info( "Verbindung zum Redis-Server wurde geschlossen." ) ) ).
-                    connect();
+  redisClient = await createClient().connect();
 }
 
 
@@ -27,11 +22,11 @@ export async function initRedisClient() {
  * Prüft, ob ein Link-Baum mit der gegebenen ID vorhanden ist.
  *
  * @param {string} linkBaumId ID von Link-Baum, dessen Existenz geprüft werden soll
- * 
+ *
  * @returns {Promise<boolean>} `true`, wenn der Link-Baum vorhanden ist, sonst `false`
  */
 export async function istLinkBaumVorhanden( linkBaumId ) {
-  
+
   const vorhanden = await redisClient.exists( `linkbaum:${linkBaumId}` );
 
   if ( vorhanden ) { return true; } else { return false; }
@@ -42,7 +37,7 @@ export async function istLinkBaumVorhanden( linkBaumId ) {
  * Speichert einen Link-Baum im Redis-Server.
  *
  * @param {string} linkBaumId ID von Link-Baum, der gespeichert werden soll
- * 
+ *
  * @param {LinkBaum} linkBaumObjekt Das Link-Baum-Objekt, das gespeichert werden soll
  */
 export async function setLinkBaum( linkBaumId, linkBaumObjekt ) {
@@ -55,7 +50,7 @@ export async function setLinkBaum( linkBaumId, linkBaumObjekt ) {
  * Holt einen Link-Baum aus dem Redis-Server.
  *
  * @param {string} linkBaumId ID von Link-Baum, der geholt werden soll
- * 
+ *
  * @returns {Promise<LinkBaum|null>} Das Link-Baum-Objekt, falls vorhanden, sonst `null`
  */
 export async function fetchLinkBaum( linkBaumId ) {
@@ -69,12 +64,12 @@ export async function fetchLinkBaum( linkBaumId ) {
 
     return null;
   }
-} 
+}
 
 
 /**
  * Zählt die Anzahl der Link-Bäume, die im Redis-Server gespeichert sind.
- * 
+ *
  * @returns Anzahl der Link-Bäume
  */
 export async function getAnzahlLinkBaeume() {
